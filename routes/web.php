f07548aae27ace3174bcad07e6609d765bdeb0d0
Rob Pungello <rob@pdfsystems.com>
2025-05-07 13:34:33 -0400

    Remove welcome view

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response('Please interact with the API through the provided endpoints.', 200);
 });
