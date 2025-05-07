e5b44cdf2261372abd2c3578fa62733c11548843
Rob Pungello <rob@pdfsystems.com>
2025-05-07 12:08:23 -0400

    Move app controller

--- app/Http/Controllers/Api/ApplicationController.php
+++ app/Http/Controllers/Api/ApplicationController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreApplicationRequest;
 use App\Http\Requests\UpdateApplicationRequest;
 use App\Models\Application;
