465b7f4123b1c9d613b9e41ffe0852f2baaf1e7d
Rob Pungello <rob@pdfsystems.com>
2025-05-07 12:15:17 -0400

    Add command to create apps

---
+++ app/Console/Commands/ApplicationCreateCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Application;
+use Illuminate\Console\Command;
+use function Laravel\Prompts\text;
+
+class ApplicationCreateCommand extends Command
+{
+    protected $signature = 'application:create';
+
+    protected $description = 'Creates a new application';
+
+    public function handle(): int
+    {
+        $name = text('Enter the name of the application', required: true);
+        $url = text('Enter the URL of the application');
+
+        $application = Application::create([
+            'name' =>  $name,
+            'url' =>  $url,
+        ]);
+
+        $this->info("Application $application->name successfully created with id {$application->getKey()}.");
+
+        return static::SUCCESS;
+    }
+}
