9e32abaa05fa76737c6e206d6f9bc78f6799c4ef
Rob Pungello <rob@pdfsystems.com>
2025-05-15 09:18:13 -0400

    Default roles.admin to false

--- database/migrations/2025_05_07_141955_create_roles_table.php
+++ database/migrations/2025_05_07_141955_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('description')->nullable();
-            $table->boolean('admin');
+            $table->boolean('admin')->default(false);
             $table->timestamps();
             $table->softDeletes();
         });
