93cb80783cf272a19d14b7b0642a1a524d7f3a9a
Rob Pungello <rob@pdfsystems.com>
2025-06-26 11:53:24 -0400

    Create some basic initial roles

--- database/seeders/RoleTableSeeder.php
+++ database/seeders/RoleTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Action;
+use App\Models\Backup;
 use App\Models\Role;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +15,30 @@ class RoleTableSeeder extends Seeder
             'name' => 'Administrator',
             'description' => 'Full access to the system',
             'admin' => true,
+        ]);
+
+        Role::create([
+            'name' => 'Backup',
+            'description' => 'Only allowed to store new backups',
+        ])->permissions()->create([
+            'resource' => Backup::class,
+            'action' => Action::Create->value,
+        ]);
+
+        Role::create([
+            'name' => 'Restore',
+            'description' => 'Only allowed to restore backups',
+        ])->permissions()->create([
+            'resource' => Backup::class,
+            'action' => Action::Read->value,
+        ]);
+
+        Role::create([
+            'name' => 'Backup Manager',
+            'description' => 'Full access to backups',
+        ])->permissions()->create([
+            'resource' => Backup::class,
+            'action' => Action::All->value,
         ]);
     }
 }
